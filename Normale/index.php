23a55c4f892348899f823afa8c1755747488d994
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-01-24 16:46:23 +0100

    add: milestone 1

--- Normale/index.php
+++ Normale/index.php
@@ -29,16 +29,22 @@
                     </div>
                 </div>
             </form>
-        </div>
-        <?php
-        if (isset($_GET['input01'])) {
+            <?php
+        if (isset($_GET['input01']) && $_GET['input01'] != '') {
             $scelta = $_GET['input01'];
-            if ($scelta != '') {
-
-                echo "Opzione selezionata: " . $scelta;
-            }
+            function GeneraPassword($numMax){
+                $password='';
+                for($i=0;$i<$numMax;$i++){
+                    $password .= chr(rand(32, 125));
+                }
+                return $password;
+            };
+            echo '<span class="password_title">Password incredibile: </span>'.( GeneraPassword($scelta));
         }
+
         ?>
+        </div>
+        
     </main>
 </body>
 
